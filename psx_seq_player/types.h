e6bc59ae61d9942a9535eec7f59e0faace18ef5c
sozud <122322823+sozud@users.noreply.github.com>
2023-12-31 17:06:28 -0500

    fixup

--- psx_seq_player/types.h
+++ psx_seq_player/types.h
@@ -10,17 +10,4 @@ typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
 
-// #ifndef PSX
-// #include <inttypes.h>
-// #else
-// typedef char s8;
-// typedef short int16_t;
-// typedef int int32_t;
-// typedef long long int64_t;
-// typedef unsigned char uint8_t;
-// typedef unsigned short uint16_t;
-// typedef unsigned int uint32_t;
-// typedef unsigned long long uint64_t;
-// #endif
-
 #endif
